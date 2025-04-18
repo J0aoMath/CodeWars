d5c545c116940c350ae643b88a2afe2afedb394d
J0aoMath <joaomath.develop@gmail.com>
2025-04-18 12:38:23 -0300

    18/04/2025

--- parseInt() reloaded.py
+++ parseInt() reloaded.py
@@ -1,45 +1,80 @@
+#In this kata we want to convert a string into an integer. The strings simply represent the numbers in words.
+#Examples:
+#"one" => 1
+#"twenty" => 20
+#"two hundred forty-six" => 246
+#"seven hundred eighty-three thousand nine hundred and nineteen" => 783919
+#Additional Notes:
+#The minimum number is "zero" (inclusively)
+#The maximum number, which must be supported is 1 million (inclusively)
+#The "and" in e.g. "one hundred and twenty-four" is optional, in some cases it's present and in others it's not
+#All tested numbers are valid, you don't need to validate them
+
+
 def transform_list(answer):
-        #[40, 4, 1000000, 3, 100, 80, 6, 1000, 1, 100, 50, 6]
-        print(f'---------------------- \n essa é a lista: {answer}')
-        others = []
-        for f,value in enumerate(answer):
-                if len(str(value)) == 2:
-                        try:
-                                if answer[f+1] < value:
-                                        answer[f] = value + answer[f+1]
-                                        answer.pop(f+1)
-                        except:
-                                pass
-        print(f'nova lista: {answer}')
-        for f,value in enumerate(answer[::-1]):
-                z = len(answer)-f
-                if value >= 100:
-                        try:
-                                if not answer[z-1] < answer[z-2]:
-                                        print(f'answer:{answer} value: {value}')
-                                        answer[z-1] = value * answer[z-2]
-                                        print(f'números sendo multiplicados: {value} e {answer[z-2]}')
-                                        answer.pop(z-2)
-                                        print(f'esse é o answer[z-2] {answer[z-2]}')
-                                elif len(str(answer[z-2])) == 3:
-                                        if value == 100:
-                                                break
-                                        print(f'answer:{answer} value: {value}')
-                                        print(f'z-3,z-2 e z-1: {answer[z-3]}x{answer[z-2]}+{answer[z-1]}')
-                                        answer[z-2] = answer[z-3] * answer[z-2] + answer[z-1]
-                                        answer.pop(z-1)
-                                        answer.pop(z-3)
-                                        answer[z-2] = answer[z-3] * answer[z-2]
-                                        answer.pop(z-3)
-                        except:
-                                try:
-                                        answer[z] = value * answer[z-2]
-                                        answer.pop(z-2)
-                                        print(f'tentou {value} * {answer[z-2]}')
-                                except:
-                                        pass
-        
-        print(f'essa é a resposta: {answer}')
+    if len(answer) == 1:
+        return sum(answer)
+    answer_ = []
+    remove_ = []
+    if len(answer) == 1:
+        return answer
+    for f,value in enumerate(answer):
+        if value == 100:
+                answer_.append(value * answer[f-1])
+                remove_.append(f-1)
+        elif 10 <= value < 100:
+            try:
+                if answer[f+1] < 10:
+                        answer_.append(value + answer[f+1])
+                        remove_.append(f+1)
+                else:
+                    answer_.append(value)
+            except:
+                answer_.append(value)
+        else:
+            answer_.append(value)
+    c = 0
+    for i in remove_:
+        try:
+            answer_.pop(i - c)
+            c += 1
+        except:
+            pass
+    answer_2 = []
+    for f,value in enumerate(answer_):
+        if 100 <= value < 1000:
+            try:
+                if answer_[f+1] < 100:
+                    answer_2.append(value+answer_[f+1])
+                else:
+                    answer_2.append(value)
+            except:
+                print(value)
+                answer_2.append(value)
+        else:
+            if value > 100:
+                answer_2.append(value)
+            elif f== len(answer_) - 1 and answer_[f-1] >= 1000:
+                    answer_2.append(value)
+            else:
+                if f == 0:
+                    answer_2.append(value)                        
+                else:
+                    pass
+    answer_.clear()
+    for f,value in enumerate(answer_2):
+        if value >= 1000:
+            try:
+                answer_.append(value * answer_2[f-1])
+            except:
+                answer_.append(value)
+        else:
+            try :
+                answer_2[f+1] += 0
+            except:
+                answer_.append(value)
+    return sum(answer_)
+                
 
         
 
@@ -63,10 +98,39 @@ def parse_int(string):
             answer_.append(decimal_[f])
         elif f in others_:
             answer_.append(others_[f])
-
     return transform_list(answer_)
 
-parse_int('one')
-parse_int('two hundred forty-six')
-parse_int('forty-four million three hundred eighty-six thousand one hundred fifty-six')
+# Others Solutions:
+ONES = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine',
+        'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 
+        'eighteen', 'nineteen']
+TENS = ['twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety']
 
+def parse_int(string):
+    print(string)
+    numbers = []
+    for token in string.replace('-', ' ').split(' '):
+        if token in ONES:
+            numbers.append(ONES.index(token))
+        elif token in TENS:
+            numbers.append((TENS.index(token) + 2) * 10)
+        elif token == 'hundred':
+            numbers[-1] *= 100
+        elif token == 'thousand':
+            numbers = [x * 1000 for x in numbers]
+        elif token == 'million':
+            numbers = [x * 1000000 for x in numbers]
+    return sum(numbers)
+#
+words = {w: n for n, w in enumerate('zero one two three four five six seven eight nine ten eleven twelve thirteen fourteen fifteen sixteen seventeen eighteen nineteen'.split())}
+words.update({w: 10 * n for n, w in enumerate('twenty thirty forty fifty sixty seventy eighty ninety hundred'.split(), 2)})
+thousands = {w: 1000 ** n for n, w in enumerate('thousand million billion trillion quadrillion quintillion sextillion septillion octillion nonillion decillion'.split(), 1)}
+def parse_int(strng):
+    num = group = 0
+    for w in strng.replace(' and ', ' ').replace('-', ' ').split():
+        if w == 'hundred': group *= words[w]
+        elif w in words: group += words[w]
+        else:
+            num += group * thousands[w]
+            group = 0
+    return num + group
